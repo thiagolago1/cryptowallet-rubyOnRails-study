c40b106d85773bf38c95c71d170dd6f7356b22b6
Thiago Lago <thiagolagoo2@gmail.com>
2021-07-17 22:20:00 +0100

    :tada: Initial Commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,19 @@
+module ApplicationHelper
+  def data_br(data_us)
+      data_us.strftime("%d/%m/%Y")
+  end
+
+  def name_application
+    "Crypto Wallet APP"
+  end
+
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Testes"
+    end
+  end
+end
